e32fe004e3b5d7815440d8c7f8c2f94f76893ff9
Oksana Astapova <astapova.frontend@gmail.com>
2022-05-15 08:52:10 +0300

    fix: fix 02-write-file

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -4,6 +4,11 @@ const path = require('path');
 const pathText = path.join(__dirname, 'text.txt');
 const output = fs.createWriteStream(pathText);
 const {stdin, stdout, exit} = process;
-stdout.write('Please, write down your text');
-stdin.on('data', data => stdout.write(data));
-process.on('exit', () => stdout.write('Good Bye!'));
+
+stdout.write('Please, write down your text.. \n');
+
+stdin.on('data', msg => {if (msg.toString().trim() === 'exit') exit();
+output.write(msg.toString());
+});
+ process.on('exit', () => stdout.write('Good Bye!'));
+ process.on('SIGINT', exit);
